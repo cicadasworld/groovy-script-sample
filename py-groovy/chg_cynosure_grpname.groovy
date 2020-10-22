258ab6639fe8d138b974170f7037b6fc72e11096
hujin <flyterren@163.com>
2020-10-22 19:37:30 +0800

    fix bug

--- py-groovy/chg_cynosure_grpname.groovy
+++ py-groovy/chg_cynosure_grpname.groovy
@@ -1,6 +1,5 @@
 /**
- * create by
- * @author hujin 2020/9/13
+ * hujin create on 2020/9/13
  */
 if (args.length != 2) {
     println "Usage: groovy ${this.class.name}.groovy <cynosuregroups.txt所在目录> <newGroupName>"
@@ -11,22 +10,24 @@ String pivotDir = args[0]
 def newGroupName = args[1]
 
 def cynosuregroupsFile = new File(pivotDir, 'cynosuregroups.txt')
-if (cynosuregroupsFile.exists()) {
+if (!cynosuregroupsFile.exists()) {
     System.err.println "ERROR: 文件不存在:$cynosuregroupsFile"
 }
 
-cynosuregroupsFile.eachLine { line ->
-    def s = line.trim()
+cynosuregroupsFile.eachLine {
+    def s = it.trim()
     if (s.startsWith('#')) return
     def fileToModify = new File(pivotDir, s)
     println "modify $fileToModify"
     def lines = []
     fileToModify.eachLine {
         if (it.startsWith('cynosure.groupName')) {
-            lines << "cynosure.groupName = $newGroupName\n"
+            lines << "cynosure.groupName = $newGroupName"
+        } else {
+        	lines << it
         }
     }
-    fileToModify.withWriter {writer ->
+    fileToModify.withWriter { writer ->
         lines.each {
             writer.writeLine it
         }
